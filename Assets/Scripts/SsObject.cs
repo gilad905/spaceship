885fcb2e0271988034bb1be258d787897bb8ee2b
Gilad Mayani <gilad905@gmail.com>
2017-04-21 01:21:12 +0200

    Added object objects, started writing classes

---
+++ Assets/Scripts/SsObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SsObject : MonoBehaviour {
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Interact();
+    }
+
+    private void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private bool PlayerIsFacing()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Tiled2Unity/Scripts/Editor/Custom/CustomTiledImporter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Tiled2Unity.CustomTiledImporter]
+public class CustomImporter_test : Tiled2Unity.ICustomTiledImporter
+{
+    public void HandleCustomProperties(GameObject gameObject, IDictionary<string, string> props)
+    {
+        if (props.ContainsKey("name"))
+        {
+            //// Remove old tile object
+            //Transform oldTileObject = gameObject.transform.Find("TileObject");
+            //if (oldTileObject != null)
+            //{
+            //    GameObject.DestroyImmediate(oldTileObject.gameObject);
+            //}
+
+            // Replace with new spawn object
+            //GameObject spawnInstance = (GameObject)GameObject.Instantiate(spawn);
+            GameObject spawnInstance = (GameObject)Object.Instantiate(Resources.Load(props["name"]));
+            //spawnInstance.name = spawn.name;
+
+            // Use the position of the game object we're attached to
+            spawnInstance.transform.parent = gameObject.transform;
+            spawnInstance.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    public void CustomizePrefab(GameObject prefab)
+    {
+        //Debug.Log("!");
+    }
+}
