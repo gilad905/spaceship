685029d564f32cdb320155f09c289e26834db4ef
Gilad Mayani <gilad905@gmail.com>
2017-04-20 02:26:30 +0200

    added custom tile importer and tile object automapping

--- Assets/git pusher.bat
+++ Assets/git pusher.bat
@@ -1,11 +1,11 @@
-rem @echo off
+@echo off
 
 set msg=%1
 IF [%1] == [] GOTO promptInput
 GOTO afterPrompt
 
 :promptInput
-set /p msg="Enter commit message (without quotes): "
+set /p msg="Enter commit message (without double quotes): "
 
 :afterPrompt
 cd "C:\Users\gilad\Documents\Programming\Gaming\Spaceship"
